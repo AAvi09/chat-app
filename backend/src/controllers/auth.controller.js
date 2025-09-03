d0153f5c9bc29f1f6525ec460d73cbd3a23e8860
Avinash sharma <sharmaavi2196@gmail.com>
2025-09-04 02:44:11 +0530

    Implement user signup functionality with password hashing and JWT generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,48 @@
 import express from "express";
+import User from "../models/users.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
 
-export const signup = (req, res) => {
-  res.json({ message: "signup route" });
+export const signup = async (req, res) => {
+  const { email, fullName, password } = req.body;
+
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email,
+      fullName,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      //generate jwt token here
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        email: newUser.email,
+        fullName: newUser.fullName,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "invalid user data" });
+    }
+  } catch (error) {
+    console.log("error in signup controller", error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 export const login = (req, res) => {
   res.send("Login route");
