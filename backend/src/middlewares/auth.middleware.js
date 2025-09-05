7a182753689a0c59fa54efa34a4f4c73fd317ece
Avinash sharma <sharmaavi2196@gmail.com>
2025-09-05 18:48:59 +0530

    Add cookie parser middleware and enhance route protection with token validation

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -9,5 +9,15 @@ export const protectRoute = async (req, res, next) => {
         .status(401)
         .json({ message: "unauthorized , no token provided" });
     }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "unauthorized , invalid token" });
+    }
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "unauthorized , user not found" });
+    }
+    req.user = user; // req.user will be available in the next middleware or route handler
+    next();
   } catch (error) {}
 };
