41a5095d31027a3c00c0954849cdefda6508625c
Avinash sharma <sharmaavi2196@gmail.com>
2025-09-13 23:41:09 +0530

    Implement message route and controller for fetching users in sidebar

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware";
+import { getUsersForSidebar } from "../controllers/message.controller.js";
 
 const router = express.Router();
 router.get("/users", protectRoute, getUsersForSidebar);
