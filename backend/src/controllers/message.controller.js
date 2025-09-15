99f8a505fa441cf64a6e5b40f7678a980f412ea7
Avinash sharma <sharmaavi2196@gmail.com>
2025-09-15 21:41:53 +0530

    Implement initial structure for getUsersForSidebar controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,1 +1,6 @@
-export const getUsersForSidebar = async (req, res) => {};
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = 
+  } catch (error) {}
+};
