7e80da6bd6b216ddf7c8239511c9f331d67d6422
Avinash sharma <sharmaavi2196@gmail.com>
2025-09-16 23:42:23 +0530

    Refactor getUsersForSidebar to properly fetch and return filtered user data

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,9 @@
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
-    const filteredUsers = 
+    const filteredUsers = Users.find({ _id: { $ne: loggedInUserId } }).select(
+      "_id name email"
+    );
+    res.status(200).json({ users: filteredUsers });
   } catch (error) {}
 };
