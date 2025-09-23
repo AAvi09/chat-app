b6fd464751267c078512ead7347085dbadfa0dac
Avinash sharma <sharmaavi2196@gmail.com>
2025-09-23 22:43:12 +0530

    Fix variable naming in getMessages function for clarity

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -15,8 +15,10 @@ export const getUsersForSidebar = async (req, res) => {
 
 export const getMessages = async (req, res) => {
   try {
-    const { id } = req.params;
-    const loggedInUserId = req.user._id;    }catch (error) {
+    const { id: userToChatId } = req.params;
+    const loggedInUserId = req.user._id;
+  } catch (error) {
     console.error("Error fetching messages:", error);
     res.status(500).json({ message: "Server Error" });
   }
+};
