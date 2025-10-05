7767eb32a517d2235a789152684dac3955ba9616
Avinash sharma <sharmaavi2196@gmail.com>
2025-10-05 23:46:40 +0530

    Update text in App component to reflect current status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-red-500">this is working pretty fine</div>;
+  return <div className="text-red-500">we are on</div>;
 };
 
 export default App;
