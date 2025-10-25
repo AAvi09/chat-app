519c47c503b162bb19add95267d7498f4b36176e
Avinash sharma <sharmaavi2196@gmail.com>
2025-10-25 22:49:20 +0530

    Add authUser retrieval in ProfilePage component

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useStoreAuth } from "../store/useStoreAuth";
 
 const ProfilePage = () => {
+  const { authUser } = useStoreAuth();
   return <div>ProfilePage</div>;
 };
 
