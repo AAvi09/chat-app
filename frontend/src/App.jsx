419df67eada09fdec795ae1e590c847cc199c1a7
Avinash sharma <sharmaavi2196@gmail.com>
2025-11-10 22:43:56 +0530

    Fix formatting of loading state return in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,11 +18,13 @@ const App = () => {
 
   console.log("Authenticated User:", authUser);
 
-  if (isCheckingAuth && !authUser) return {
-    <div className = "flex items-center justify-center min-h-screen">
-    <Loader className = "size-10 animate-spin"/>
-    </div>
-  };
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
     <div>
       <Navbar />
