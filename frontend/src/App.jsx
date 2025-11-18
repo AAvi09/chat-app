85c9240272b4328df6489cd5095240ed3d32ee2d
Avinash sharma <sharmaavi2196@gmail.com>
2025-11-18 21:29:00 +0530

    Fix profile route logic to redirect unauthenticated users to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route
           path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/" />}
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
         />
       </Routes>
     </div>
