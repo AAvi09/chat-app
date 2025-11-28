d3e349f2d4812f0e103e0bf7206404f279310277
Avinash sharma <sharmaavi2196@gmail.com>
2025-11-28 23:02:28 +0530

    Enhance layout structure in SignUpPage component by adding image section

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -16,7 +16,12 @@ const SignUpPage = () => {
     e.preventDefault();
     signUp(formData);
   };
-  return <div className="min-h-screen grid lg:grid-cols-2"></div>;
+  return (
+    <div className="min-h-screen grid lg:grid-cols-2">
+      {/* Left Side - Image Section */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12"></div>
+    </div>
+  );
 };
 
 export default SignUpPage;
