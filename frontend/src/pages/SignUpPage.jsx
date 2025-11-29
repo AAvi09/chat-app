026118091f9ef3b97196567ac9f3a989fbd33d1f
Avinash sharma <sharmaavi2196@gmail.com>
2025-11-29 23:19:44 +0530

    Enhance layout structure in SignUpPage component by adding content section

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -19,7 +19,9 @@ const SignUpPage = () => {
   return (
     <div className="min-h-screen grid lg:grid-cols-2">
       {/* Left Side - Image Section */}
-      <div className="flex flex-col justify-center items-center p-6 sm:p-12"></div>
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-wd space-y-8"></div>
+      </div>
     </div>
   );
 };
