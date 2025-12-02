540d0cb8c9d26e025f5924d9d2041d59f9702bbe
Avinash sharma <sharmaavi2196@gmail.com>
2025-12-02 21:09:17 +0530

    Enhance layout structure in SignUpPage component by adding a visual element in the image section

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -22,7 +22,9 @@ const SignUpPage = () => {
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-wd space-y-8">
           <div className="text-center mb-8">
-            <div className="flex flex-col items-center gap-2 group"></div>
+            <div className="flex flex-col items-center gap-2 group">
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors"></div>
+            </div>
           </div>
         </div>
       </div>
