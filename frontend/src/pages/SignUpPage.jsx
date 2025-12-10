530b57ae853ff2eb15efadbfa1e6687b5ae2a5d1
Avinash sharma <sharmaavi2196@gmail.com>
2025-12-10 22:41:35 +0530

    Enhance layout structure in SignUpPage component by adding input field for user Full Name

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -41,6 +41,7 @@ const SignUpPage = () => {
                 <div className="absolute inset-y-0 left-0 pl-3 flex  items-center pointer-events-none">
                   <User className="size-5 text-base-content/40" />
                 </div>
+                <input type="text" />
               </div>
             </div>
           </form>
